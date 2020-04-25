5b830310682a3decccaf46ee0e24ca4122831fa1
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-04-25 13:56:14 +0530

    ➕ Added UI + Todo Functionality

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos" />
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
-
 <script>
+import Header from "./components/layouts/Header";
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 export default {
   name: "App",
   components: {
-    Todos
+    Todos,
+    Header,
+    AddTodo,
   },
   data() {
     return {
@@ -18,21 +23,29 @@ export default {
         {
           id: 1,
           title: "Todo One",
-          completed: false
+          completed: false,
         },
         {
           id: 2,
           title: "Todo Two",
-          completed: true
+          completed: true,
         },
         {
           id: 3,
           title: "Todo Three",
-          completed: false
-        }
-      ]
+          completed: false,
+        },
+      ],
     };
-  }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
+  },
 };
 </script>
 
@@ -41,5 +54,23 @@ export default {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
